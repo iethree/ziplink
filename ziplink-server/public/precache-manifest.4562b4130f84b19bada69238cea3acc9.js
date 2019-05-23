a7bb219ed265ce6729c245e4631f075693c9d0b4
Ryan Laurie <rlaurie@GAIRLAW.local>
2019-05-23 16:43:26 -0500

    implement box auth

---
+++ ziplink-server/public/precache-manifest.4562b4130f84b19bada69238cea3acc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2323a861fb10f05034d",
+    "url": "/static/css/main.d3da41ae.chunk.css"
+  },
+  {
+    "revision": "b2323a861fb10f05034d",
+    "url": "/static/js/main.b2323a86.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c96e88f077c5841a5a64",
+    "url": "/static/js/2.c96e88f0.chunk.js"
+  },
+  {
+    "revision": "36f16618b6fb448c2c3eb9fada2cb6f4",
+    "url": "/index.html"
+  }
+];
--- ziplink-server/ziplink-routes.js
+++ ziplink-server/ziplink-routes.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const crypto = require('crypto');
@@ -9,18 +8,22 @@ const fs=require('fs');
 const BoxSDK = require('box-node-sdk');
 
 //Box Credentials
-// var sdk = new BoxSDK({
-//   clientID: 'CLIENT_ID',
-//   clientSecret: 'CLIENT_SECRET'
-// });
-// var client = sdk.getBasicClient('DEVELOPER_TOKEN');
-// var folderID;
+var config =JSON.parse(fs.readFileSync('.env'));
+
+//const sdk = BoxSDK.getPreconfiguredInstance(config);
+const sdk = new BoxSDK({
+  clientID: config.boxAppSettings.clientID,
+  clientSecret: config.boxAppSettings.clientSecret
+});
+//const client = sdk.getAppAuthClient('enterprise', config.enterpriseID);
+const client = sdk.getBasicClient('uGqM7V5JUpuoxRvxuAFMaumEkwBdgNOu');
+
 // client.folders.create('0', 'Ziplink files').then((folder)=>{
 //   folderID=folder.id;
 // });
-var client;
 
 const tempPath ='docTemp/';
+const savePath ='uploads/';
 
 const multer = require('multer');
 var upload = multer({ dest:tempPath });
@@ -29,44 +32,62 @@ var upload = multer({ dest:tempPath });
 router.post('/uploadDocs', upload.array('file',99), function(req, res, next){
 
 	console.log(chalk.blue(req.files.length+" files uploaded"));
-
+  var fileList=[];
   //zip files
-  var fileList = req.files.map(file=>'"'+file.path+"'");
-  var zipName = "zip_"+Date.now()+".zip";
-  shell.exec(`zip -m ${zipName} ${fileList.join(' ')} `);
+  for(let file of req.files){
+    try{
+      fs.renameSync(file.path, savePath+file.originalname);
+      fileList.push('"'+savePath+file.originalname+'"');
+    }
+    catch(e){
+      console.log(chalk.red(e));
+    }
+  }
+
+  var zipName = req.body.name ? savePath+req.body.name+".zip" : savePath+"zip_"+Date.now()+".zip";
+  let cmd=`zip -m ${zipName} ${fileList.join(' ')} `;
+  console.log(cmd);
+  shell.exec(`zip -m ${zipName} ${fileList.join(' ')} `, {silent: false}, (code, stdout, stderr)=>{
+    if(code===0) console.log(chalk.green('zipped'));
+    else console.log(chalk.red('unexpected exit code on zipping'));
+
+    var password = crypto.randomBytes(12).toString('base64');
+    var expireDays = req.body.expires || 14;
+    var expires = datefns.addDays(new Date(), expireDays);
+
+    var stream = fs.createReadStream(savePath+zipName);
+    var folderID = '77412689480';
+    console.log(chalk.blue('uploading: '+zipName));
+
+    client.files.uploadFile(folderID, zipName, stream)
+    .then(file => {
+      client.files.update(file.id, {
+          shared_link:{
+            access:'open',
+            password: password,
+            unshared_at: expires
+          }
+      }).then(updatedFile=>{
+          console.log(chalk.green('file ready: '+updatedFile.name));
+
+          //send link and password back to client
+          res.send({
+            link: updatedFile.shared_link.download_url,
+            password: password,
+            expires: expireDays
+          }).status(200);
+
+          shell.rm(zipFile);
+      });
+    });
+  });
 
   //handle zip upload already
 
+    /* WIP */
+
   //send to box
-  var password = crypto.randomBytes(12).toString('base64');
-  var expireDays = 14;
-  var expires = datefns.addDays(new Date(), expireDays);
 
-  var stream = fs.createReadStream(zipName);
-  var folderID = "???????";
-  console.log(chalk.blue('uploading: '+zipName));
-
-  client.files.uploadFile(folderID, zipName , stream)
-  .then(file => {
-    client.files.update(file.id, {
-        shared_link:{
-          access:'open',
-          password: password,
-          unshared_at: expires
-        }
-    }).then(updatedFile=>{
-        console.log(chalk.green('file ready: '+updatedFile.name));
-
-        //send link and password back to client
-        res.send({
-          link: updatedFile.shared_link.download_url,
-          password: password,
-          expires: expireDays
-        }).status(200);
-
-        shell.rm(zipFile);
-    });
-  });
 });
 
 module.exports = router;
